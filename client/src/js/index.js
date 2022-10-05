0b48504569aeae52b0e40b3738ece98430eb0e83
Cristina Dege <cris.dege0813@gmail.com>
2022-10-05 19:53:19 -0400

    indexeddb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,6 @@
-import "./form";
-import "./submit";
+import { initdb, getDb, postDb } from './database';
+import './form';
+import './submit';
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,10 +9,14 @@ import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
-  });
+  initdb();
+  getDb();
+  postDb('Lernantino', 'learnantino@test.com', 8186601234, 'Bear');
+  getDb();
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
 
-    // Import CSS files
-    import "../css/index.css";
+// Import CSS files
+import '../css/index.css';
